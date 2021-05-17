8f4d144e94b2cd35a7d6dd783f758b25cd8d6515
Ergonomie <amh@ergonomies-macbook-pro.local>
2021-05-17 20:05:25 +0000

    Add experiement with dynamic skip links + Add design toggle

--- behaviour.js
+++ behaviour.js
@@ -1,17 +1,61 @@
-// https://dommagnifi.co/2016-05-16-basic-class-toggle-with-vanilla-js/
+// Simple prototype for a dynamic skip link navigation see https://codepen.io/formspiel/pen/XWpyLwO
+// Developer needed to make it ready to go live
 
-var arr = document.getElementsByClassname("js-add-style js-change-layout");
+// looks like jQuery but it's 'Cash' ;-) ==> https://github.com/fabiospampinato/cashs
 
-for (var k = 0; k < arr.length; k++)
-    arr[k].addEventListener(yourListener);
+$(function () {
+    $('html').removeClass('no-js');
+    
+    // HELPER
+    $.fn.hasAttr = function (name) {
+        return this.attr(name) !== undefined;
+    };
+    
+    // DESIGN TOGGLE
+    $('#design-01').change(function(){
+        if($(this).is(":checked")) {
+            console.log("design 01 on");
+            $('html').addClass("design-01");
+        } else {
+            console.log("design 01 OFF");
+            $('html').removeClass("design-01");
+        }
+    });
+    
+    // DYNAMIC SKIP LINKS
+    $('legend')
+    .attr("tabindex", "-1")
+    .removeAttr("title")
+    .each(function () {
+      var elementname = $(this).get(0).tagName.toLowerCase();
+      var getElementAriaLabel = $(this).attr("aria-label");
 
+      if ($(this).hasAttr("aria-label")) {
+        //console.log("I've an aria label");
+        console.log("label " + elementname + " " + getElementAriaLabel);
+        var elementAriaLabel = getElementAriaLabel;
+      } else {
+        console.log("no label for " + elementname);
+      }
 
-//var el = document.querySelector('.js-add-style');
+      if ($(this).hasAttr("id")) {
+        // console.log("true");
+      } else {
+        // console.log("false " + elementname);
+        $(this).attr("id", elementname);
+        $("#js-nav-skip-links").append(
+          '<li><a href="#' +
+            elementname +
+            '">Go to ' +
+            elementname +
+            " " +
+            elementAriaLabel +
+            "</a></li>"
+        );
+      }
+    });
 
-el.onclick = function () {
-    el.classList.toggle('active');
-}
-
+});
 
 //.js-add-style
 //.js-change-layout
